891eb3d0c76f9eb8e43be89417e9337cfb1fa88f
Keerthana Peddireddy <107753345+pekeerthana@users.noreply.github.com>
2026-05-01 15:48:03 +0530

    Add: trapping-rain-water - submission-3

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-3.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+
+        if(n==0) return 0;
+
+        Stack<Integer> stack = new Stack<>();
+        int res =0;
+
+        for(int i=0;i<n;i++){
+            while(!stack.isEmpty() && height[i] >= height[stack.peek()])
+            {
+                int mid = height[stack.pop()];
+                if(!stack.isEmpty()){
+                    int right = height[i];
+                    int left = height[stack.peek()];
+                    int h = Math.min(right,left) - mid;
+                    int w = i- stack.peek()-1;
+                    res+= h*w;
+                }
+            }
+            stack.push(i);
+        }
+
+
+        return res;
+        
+    }
+}
