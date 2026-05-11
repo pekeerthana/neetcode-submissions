5dc849c273a9aa44feeabcf084f2e5f79b69ce9d
Keerthana Peddireddy <107753345+pekeerthana@users.noreply.github.com>
2026-05-11 22:00:15 +0530

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+
+        Stack<Character> chars = new Stack<>();
+        Map<Character,Character> map = new HashMap<>();
+        map.put(']','[');  map.put('}','{');  map.put(')','(');
+
+        for(char c: s.toCharArray()){
+
+           if(chars.size() != 0 && (c ==']'||c=='}' || c==')') ){
+
+                if(map.get(c) == chars.peek()) chars.pop();
+                else return false;
+           }
+           else{chars.push(c);}
+
+        }
+
+        return chars.isEmpty();
+        
+    }
+}
